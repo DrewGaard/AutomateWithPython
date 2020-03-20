dcb07a0c94998f871e085e31d601d57657b8f30f
Drew <drew@drews-mbp.lan>
2020-03-20 14:44:44 -0700

    More debugging

--- Logging.py
+++ Logging.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 logging.disable(logging.CRITICAL)
 
